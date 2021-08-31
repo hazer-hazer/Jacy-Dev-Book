d97c2df0d8da877c9a394ac440e2864db1617f0a
is_gordeev <gordeev.john@gmail.com>
2021-08-31 23:15:58 +0300

    Update `gen.js` script

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
+const path = require('path')
+
 const SOURCE_DIR = 'src'
+const DIST_DIR = 'dist'
 
-const STRUCTURE = {
+const SOURCE_PATH = path.join(__dirname, SOURCE_DIR)
+const DIST_PATH = path.join(__dirname, DIST_DIR)
 
+module.exports = {
+    SOURCE_PATH,
+    DIST_PATH,
 }
--- gen.js
+++ gen.js
@@ -2,8 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const {
     STRUCTURE,
-    SOURCE_DIR,
+    SOURCE_PATH,
+    DIST_PATH,
 } = require('./config')
+const STRUCT = require('./struct')
 
 const capitalize = str => str[0].toUpperCase() + str.slice(1)
 
@@ -23,8 +25,8 @@ const nameFromFilename = filename => {
     return res.join(' ')
 }
 
-/// Collects all source files, auto-generates config and merges it with config described in `STRUCTURE`
-const collectSources = async (p = '', struct = STRUCTURE) => {
+/// Collects all source files, auto-generates config and merges it with config described in `struct.js`
+const collectSources = async (p = SOURCE_PATH, struct = STRUCT) => {
     const result = {}
     const entities = fs.readdirSync(p)
 
@@ -46,21 +48,25 @@ const collectSources = async (p = '', struct = STRUCTURE) => {
                 throw Error(`Invalid structure: ${p} has children but exists as file`)
             }
 
+            const src = fs.readFileSync(childPath)
+
             result[childName] = {
                 isDir: false,
                 name: subStruct.name || nameFromFilename(childName),
-                parentName: parentName
+                parentName: parentName,
+                src,
+                path: path.relative(SOURCE_PATH, childPath),
             }
         }
 
         // Ignore non-markdown files
     }
-    
+
     return result
 }
 
 ;(async () => {
-    const sources = await collectSources(path.join(__dirname, SOURCE_DIR))
+    const sources = await collectSources(SOURCE_PATH)
 
     console.log(`Sources:`, JSON.stringify(sources, null, 2));
 })()
