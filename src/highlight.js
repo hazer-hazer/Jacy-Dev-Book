95bdc8a543202fd347989e3007f3fd29f4dccf27
is_gordeev <gordeev.john@gmail.com>
2021-09-28 23:17:43 +0300

    Update styles and code fences

--- src/highlight.js
+++ src/highlight.js
@@ -6,7 +6,7 @@ const getSourceLines = src => src.split(/\r\n|\r|\n/g)
 
 const addLineNum = (num, line) => {
     return `
-        <tr><td class="line-num-col"><div class="line-num" data-line-num="${num}"></div></td><td class="line-col"><div class="line-content">${line}</div></td></tr>
+        <div class="line"><div class="line-num" data-line-num="${num}">${num}</div><div class="line-content">${line}</div></div>
     `.trim()
 }
 
@@ -18,17 +18,15 @@ const addLineNumbers = src => {
         result += addLineNum(i + 1, lines[i])
     }
 
-    return `
-        <table class="code-table">${result}</table>
-    `.trim()
+    return result
 }
 
 module.exports = src => {
     return src.replace(/```jc([\s\S]*?)```/g, (match, code) => {
         return `
-        <pre class="code-fence highlight-jc hljs">
+        <div class="code-fence highlight-jc hljs">
             ${addLineNumbers(hljs.highlight(code, {language: 'jc'}).value)}
-        </pre>
+        </div>
         `.trim()
     })
 }
