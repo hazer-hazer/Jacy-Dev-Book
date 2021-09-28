c7a5b5f006258b761e6c7dfe5f55ca5495ad0f09
hazer-hazer <gordeev.john@gmail.com>
2021-09-28 17:24:56 +0300

    Update structure

--- struct.js
+++ struct.js
@@ -1,3 +1,9 @@
 module.exports = {
-    title: 'Table of contents'
+    title: 'Table of contents',
+    children: {
+        'how-to-run': {
+            order: 1,
+            title: 'How To Run',
+        }
+    }
 }
