f0fab285a41472a897fe95c9452c00e57f0a97b0
is_gordeev <gordeev.john@gmail.com>
2021-10-03 20:27:10 +0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,13 @@
-$('.code-fence > .copy').on('click', function (el) {
-    const code = $(this).parent().find('.line').text()
-
-    const bufferInput = document.createElement('input')
-
-    bufferInput.innerText = code
-    bufferInput.focus()
-
-    console.log('copied', bufferInput.innerText);
-    document.execCommand('copy')
+Zepto($ => {
+    $('.code-fence > .copy').on('click', function (el) {
+        const code = $(this).parent().find('.line').text()
+    
+        const bufferInput = document.createElement('input')
+    
+        bufferInput.innerText = code
+        bufferInput.focus()
+    
+        console.log('copied', bufferInput.innerText);
+        document.execCommand('copy')
+    })
 })
